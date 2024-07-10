9c136501dcd36de5c433786252ffaf90ab1ffa15
hyowon.moon <hyowon.moon@samsung.com>
2024-07-10 14:50:43 +0900

    [feature] page 39

--- test_game.py
+++ test_game.py
@@ -19,6 +19,8 @@ class TestGame(TestCase):
         self.assert_illegal_argument(None)
         self.assert_illegal_argument("12")
         self.assert_illegal_argument("1234")
+        self.assert_illegal_argument("12s")
+
 
     def test_game(self):
         self.assertEqual(1, 1)
